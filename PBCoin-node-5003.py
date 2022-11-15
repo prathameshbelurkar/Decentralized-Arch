9e9b236d4e00763d7e8777f8837457f5dac89cb9
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-11-15 16:07:30 +0530

    add receiver name

--- PBCoin-node-5003.py
+++ PBCoin-node-5003.py
@@ -115,7 +115,7 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
-    blockchain.add_transaction(sender=node_address, receiver='Prathamesh', amount=1)
+    blockchain.add_transaction(sender=node_address, receiver='Kirill', amount=1)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
