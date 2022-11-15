cf9f6bc0f66c73bc2b8b498ad5eb18f70f886b9d
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-11-15 15:38:30 +0530

    add post: add and connect a node

--- main.py
+++ main.py
@@ -155,6 +155,23 @@ def add_transaction():
 
 # Decentralizing our Blockchain 
 
+# Connecting new nodes 
+@app.route('/connect_node', methods=['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {
+        "message": "All the nodes are now connected. The PBCoin Blockchain now contains the following nodes:",
+        "total_nodes": list(blockchain.nodes)
+    }
+    return jsonify(response), 201
+
+
+
 
 
 
