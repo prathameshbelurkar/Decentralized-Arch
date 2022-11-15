8a446f421c0c57265caff157ed7d46e3d7220c89
Prathamesh Belurkar <76102350+prathameshbelurkar@users.noreply.github.com>
2022-11-15 11:23:56 +0530

    add transactions to block

--- main.py
+++ main.py
@@ -102,6 +102,9 @@ class Blockchain:
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
+# Creating an address for the node on Port 5000
+node_address = str(uuid4()).replace('-', '')
+
 # Creating a Blockchain
 blockchain = Blockchain()
 
@@ -113,11 +116,13 @@ def mine_block():
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
     block = blockchain.create_block(proof, previous_hash)
+    blockchain.add_transaction(sender=node_address, reciever='Prathamesh', amount=1)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
+                'previous_hash': block['previous_hash'],
+                'transactions': block['transactions']}
     return jsonify(response),  200
 
 # Getting the full blockchain 
